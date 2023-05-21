c7d196a21589f5bcdd963e04d74ec1cb242c67f5
duccdinh16 <duccdinh16@gmail.com>
2023-05-21 20:55:46 +0700

    Update all

--- generateData.py
+++ generateData.py
@@ -1,19 +1,41 @@
 import random
 
 
-def generate_unique_numbers(n):
-    numbers = list(range(1, n + 1))
-    random.shuffle(numbers)
-    return numbers
+def generate_increasing_data(size):
+    return list(range(1, size + 1))
 
 
-num_sets = 10
-num_elements = 1000000
+def generate_decreasing_data(size):
+    return list(range(size, 0, -1))
 
-for i in range(num_sets):
-    data_set = generate_unique_numbers(num_elements)
-    file_name = f"data_{i+1}.txt"
-    with open(file_name, "w") as file:
-        for number in data_set:
+
+def generate_dataset(num_datasets, dataset_size):
+    datasets = []
+    for i in range(1, num_datasets + 1):
+        if i == 1:
+            dataset = generate_increasing_data(dataset_size)
+        elif i == 10:
+            dataset = generate_decreasing_data(dataset_size)
+        else:
+            # Generate random dataset of unique integers
+            dataset = list(range(1, dataset_size + 1))
+            random.shuffle(dataset)
+
+        datasets.append(dataset)
+
+    return datasets
+
+
+def write_dataset_to_file(filename, dataset):
+    with open(filename, "w") as file:
+        for number in dataset:
             file.write(str(number) + "\n")
-    print(f"Data set {i+1} exported to {file_name}.")
+
+
+num_datasets = 10
+dataset_size = 1000000
+
+for i, dataset in enumerate(generate_dataset(num_datasets, dataset_size)):
+    output_file = f"data_{i+1}.txt"
+    write_dataset_to_file(output_file, dataset)
+    print("Dữ liệu đã được ghi vào file", output_file)
